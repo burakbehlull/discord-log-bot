de85910c7df27ccafe5f1a39921b7de2594064ee
burakbehlull <burakyabgu@gmail.com>
2024-05-31 17:14:01 +0300

    feat: Add ChannelDelete action to function

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,19 @@ client.on('channelCreate', async (channel:NonThreadGuildBasedChannel)=> {
     const footer = { text: user.executor.displayName, iconURL: user.executor.avatarURL()}
     const Embed :any= new EmbedBuilder(sender.embed({title: "Channel Log", footer}))
         .setDescription(`<@${user.executorId}> adlı kullanıcı <#${channel.id}> kanalını oluşturdu.`)
-        
+        .setThumbnail(channel.guild.iconURL())
+        sender.send(Embed, "1245026894165053590")
+    })
+
+client.on('channelDelete', async (channel:NonThreadGuildBasedChannel)=> {
+    const user = await sender.info(channel, AuditLogEvent.ChannelDelete)
+    const footer = { text: user.executor.displayName, iconURL: user.executor.avatarURL()}
+    const Embed :any= new EmbedBuilder(sender.embed({title: "Channel Log", footer}))
+        .setDescription(`<@${user.executorId}> adlı kullanıcı ${channel.name} (id: ${channel.id}) adlı kanalı sildi.`)
+        .setThumbnail(channel.guild.iconURL())
     sender.send(Embed, "1245026894165053590")
 })
+
 
 client.login(process.env.TOKEN)
 .then(()=> console.log('Bot başlatıldı'))
