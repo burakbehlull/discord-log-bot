bf39a71dfcb60331b2c2885b7defabd1c2cc8447
burakbehlull <burakyabgu@gmail.com>
2024-06-07 16:57:19 +0300

    feat: Automate events folder

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,20 @@
-import {PresenceData} from 'discord.js'
+import {PresenceData, ActivityType} from 'discord.js'
 
-export function ready(client: any | undefined, activities:PresenceData){
-    if(!client) return
-    client.on('ready', ()=> {
-        client.user.setPresence(activities)
-    })
+export default {
+    name: 'hazır',
+    func: (client: any | undefined) =>{
+        if(!client) return
+        client.on('ready', ()=> {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: 'Star Wars Clon Savaşları', 
+                        type: ActivityType.Watching
+                    }
+                ],
+                status: 'dnd',
+                shardId: 0
+            })
+        })
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import { Client, GatewayIntentBits, AuditLogEvent, 
-    NonThreadGuildBasedChannel, EmbedBuilder, 
-    ActivityType} from 'discord.js'
+    NonThreadGuildBasedChannel, EmbedBuilder } from 'discord.js'
+    
+import { fileURLToPath, pathToFileURL } from 'node:url'
 import dotenv from 'dotenv'
-import {messageSender} from './helpers/index.js'
-import {ready} from './events/index.js'
+import path from 'node:path'
+import fs from 'fs'
+
+import { messageSender } from './helpers/index.js'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const eventsPath = path.join(dirname, 'events')
+const eventsFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+
+(async () => {
+  for (const file of eventsFiles) {
+    const filePath = path.join(eventsPath, file)
+    const fileUrl = pathToFileURL(filePath).href
+    const event = await import(fileUrl)
+    console.log(`${event.default?.name} yüklendi..`)
+    if(event.default?.func){
+        event.default?.func(client)
+    }
+  }
+})()
+
 dotenv.config()
 
 const client = new Client({
@@ -20,17 +41,6 @@ const client = new Client({
 
 const sender = new messageSender(client)
 
-ready(client, {
-    activities: [
-        {
-            name: 'Star Wars Clon Savaşları', 
-            type: ActivityType.Watching
-        }
-    ],
-    status: 'dnd',
-    shardId: 0
-})
-
 client.on('channelCreate', async (channel:NonThreadGuildBasedChannel)=> {
     const user = await sender.info(channel, AuditLogEvent.ChannelCreate)
     const footer = { text: user.executor.displayName, iconURL: user.executor.avatarURL()}
@@ -43,11 +53,12 @@ client.on('channelCreate', async (channel:NonThreadGuildBasedChannel)=> {
 client.on('channelDelete', async (channel:NonThreadGuildBasedChannel)=> {
     const user = await sender.info(channel, AuditLogEvent.ChannelDelete)
     const footer = { text: user.executor.displayName, iconURL: user.executor.avatarURL()}
-    const Embed :any= new EmbedBuilder(sender.embed({title: "Channel Log", footer}))
+    const Embed :any = new EmbedBuilder(sender.embed({title: "Channel Log", footer}))
         .setDescription(`<@${user.executorId}> adlı kullanıcı, **${channel.name}** (id: ${channel.id}) adlı kanalı sildi.`)
         .setThumbnail(channel.guild.iconURL())
     sender.send(Embed, "1245026894165053590")
 })
+
 
 client.on('channelUpdate', async (channel:NonThreadGuildBasedChannel)=> {
     const user = await sender.info(channel, AuditLogEvent.ChannelUpdate)
@@ -58,6 +69,11 @@ client.on('channelUpdate', async (channel:NonThreadGuildBasedChannel)=> {
     sender.send(Embed, "1245026894165053590")
 })
 
+
+
 client.login(process.env.TOKEN)
-.then(()=> console.log('Bot başlatıldı'))
+.then(()=> {
+    console.log('Bot başlatıldı')
+
+})
 .catch((err)=> console.log('error: ', err))
