134153f10cdcbb28eaf6ea0c5448068836264a0f
burak behlül <burakyabgu@gmail.com>
2025-05-26 15:10:23 +0300

    feat: Add new features

--- src/events/channelDelete.ts
+++ src/events/channelDelete.ts
@@ -1,17 +1,15 @@
 import { AuditLogEvent, EmbedBuilder, NonThreadGuildBasedChannel } from 'discord.js'
 import { messageSender } from '../helpers/index.js'
 
-
 export default {
-    name: 'Channel Create Event',
+    name: 'Channel Delete Event',
     func: (client:any)=>{
         const sender = new messageSender(client)
-        client.on('channelCreate', async (channel:NonThreadGuildBasedChannel)=> {
-			
-            const user = await sender.info(channel, AuditLogEvent.ChannelCreate)
+        client.on('channelDelete', async (channel:NonThreadGuildBasedChannel)=> {
+            const user = await sender.info(channel, AuditLogEvent.ChannelDelete)
             const footer = { text: user.executor.displayName, iconURL: user.executor.avatarURL()}
             const Embed :any = new EmbedBuilder(sender.embed({title: "Channel Log", footer}))
-                .setDescription(`<@${user.executorId}> adlı kullanıcı, **${channel.name}** (id: ${channel.id}) adlı kanalı yarattı.`)
+                .setDescription(`<@${user.executorId}> adlı kullanıcı, **${channel.name}** (id: ${channel.id}) adlı kanalı sildi.`)
                 .setThumbnail(channel.guild.iconURL())
             sender.send(Embed, "948696953695383643")
         })
